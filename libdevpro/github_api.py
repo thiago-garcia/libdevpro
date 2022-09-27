082cfd1f59fa07f9e402c0a8ec10079aeec5b758
Thiago Garcia <thiago-garcia@outlook.com>
2022-09-26 22:29:27 -0300

    Criado módulo para acessar API do GitHub close #7

---
+++ libdevpro/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário do GitHub
+
+    :param usuario: str com nome do usuário no GitHub
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('thiago-garcia'))
