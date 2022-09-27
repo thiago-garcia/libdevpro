99f77e83da10eef0a8c387ed2dd72cb95af2bef0
Thiago Garcia <thiago-garcia@outlook.com>
2022-09-26 22:37:29 -0300

    Correção de qualidade da escrita após rodar flake8

--- libdevpro/github_api.py
+++ libdevpro/github_api.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def buscar_avatar(usuario):
     """
     Busca o avatar de um usuário do GitHub
@@ -10,6 +11,7 @@ def buscar_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
 
 if __name__ == '__main__':
     print(buscar_avatar('thiago-garcia'))
