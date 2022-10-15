0cb151af21a5e9f896fa5c1a93556ba3c1de278f
Thiago Garcia <thiago-garcia@outlook.com>
2022-10-15 01:25:06 -0300

    Criado teste de envio para base e conftest fixtures close #27

---
+++ libdevpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -0,0 +1,11 @@
+from libdevpro.spam.enviador_de_email import Enviador
+from libdevpro.spam.main import EnviadorDeSpam
+
+
+def test_envio_de_spam(sessao):
+    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+    enviador_de_spam.enviar(
+        'thiago-garcia@outlook.com',
+        'Curso DevPro',
+        'Confira os módulos fantásticos'
+    )
