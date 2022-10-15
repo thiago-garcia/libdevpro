1817490737ef36a7f02549658c94775609988127
Thiago Garcia <thiago-garcia@outlook.com>
2022-10-15 01:44:22 -0300

    Adicionado o campo email ao Usuário close #29

--- libdevpro/tests/test_spam/test_usuarios.py
+++ libdevpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libdevpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Thiago')
+    usuario = Usuario(nome='Thiago', email='thiago@email.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Thiago'), Usuario(nome='Felipe')]
+    usuarios = [
+        Usuario(nome='Thiago', email='thiago@email.com'),
+        Usuario(nome='Felipe', email='felipe@email.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
