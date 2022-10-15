993217cf1449b7b6c590d6a48759d208d821d22e
Thiago Garcia <thiago-garcia@outlook.com>
2022-10-15 02:01:22 -0300

    Criado teste de qtd de spam close #31

--- libdevpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libdevpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,29 @@
+import pytest
 from libdevpro.spam.enviador_de_email import Enviador
 from libdevpro.spam.main import EnviadorDeSpam
+from libdevpro.spam.modelos import Usuario
 
 
-def test_qde_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Thiago', email='thiago@email.com'),
+            Usuario(nome='Felipe', email='felipe@email.com')
+        ],
+        [
+            Usuario(nome='Thiago', email='thiago@email.com'),
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar(
         'thiago-garcia@outlook.com',
         'Curso DevPro',
         'Confira os módulos fantásticos'
     )
+    assert len(usuarios) == enviador.qtd_emails_enviados
