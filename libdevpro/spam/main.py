cc5dfe95739a56104f8143c5845949f2ee19b0ab
Thiago Garcia <thiago-garcia@outlook.com>
2022-10-15 01:25:06 -0300

    Criado teste de envio para base e conftest fixtures close #27

---
+++ libdevpro/spam/main.py
@@ -0,0 +1,7 @@
+class EnviadorDeSpam:
+    def __init__(self, sessao, enviador):
+        self.sessao = sessao
+        self.enviador = enviador
+
+    def enviar(self, remetente, assunto, corpo):
+        pass
