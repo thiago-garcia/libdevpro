af148ace7c7f5d693fa6744adbfee8c22e201f75
Thiago Garcia <thiago-garcia@outlook.com>
2022-10-16 01:31:29 -0300

    Criado teste de integração de buscar avatar close #37

--- libdevpro/tests/test_spam/test_github_api.py
+++ libdevpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,28 @@
 from unittest.mock import Mock
+import pytest
 
 from libdevpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/13400040?v=4'
     resp_mock.json.return_value = {
         'login': 'thiago-garcia', 'id': 13400040,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/13400040?v=4',
+        'avatar_url': url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('thiago-garcia')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('thiago-garcia')
     assert 'https://avatars.githubusercontent.com/u/13400040?v=4' == url
